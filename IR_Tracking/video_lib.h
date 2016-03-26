0d0b2066456612d325e37b7e847462a6b5649b22
seokg <seokg1023@gmail.com>
2016-03-26 16:37:52 +0900

    update

--- IR_Tracking/video_lib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "use_opencv.h"
-class Video
-{
-public:
-	static bool openvideo(cv::VideoCapture cap);
-	static bool readvideo(cv::VideoCapture cap, cv::Mat& frame);
-	static void closevideo(cv::VideoCapture cap);
-};
